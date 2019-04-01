5f1750c4404ba92e803a40156944f15e7f5670ba
Bavikar <shital.r.bavikar@accenture.com>
2019-04-01 15:15:21 +0100

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+
+
+
+// Action creator 
+export const selectSong = () => {
+//Return an action
+ return {
+     type: 'SONG_SELECTED',
+     payload: song
+ }
+}
+
+ 
---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+
+import {combinedReducers} from 'redux';
+const songsReducers = () => {
+
+    return [
+        {title: 'No scrubs', duration: '4.05'},
+        {title: 'Macrena', duration: '2.50'},
+        {title: 'All stars', duration:'3.10'},
+        {title: 'I want it that way', duration: '2.60'}
+    ]
+}
+
+const selectedSongReducer = (selectedSong= null, action) => {
+  if(action.type === 'SONG_SELECTED'){
+     return action.payload;
+  }
+  return selectedSong;
+}
+
+export default combinedReducers({
+    songs: songsReducers,
+    selectedSong: selectedSongReducer
+})
